5245baa0d36fa472435d73c441efe47dfa86a528
Heber Mizuno <heber_mm@hotmail.com>
2022-02-20 12:42:28 -0300

    meu primeiro commit

---
+++ arquivo1.js
@@ -0,0 +1,14 @@
+console.log("Olá Mundo");
+
+//git init
+//git config user.name "Heber Mizuno"
+//git config user.email "heber_mm@hotmail.com"
+
+//verificar parâmetros nome e e-mail
+// git config user.name ou email
+
+//git remote add origin https://github.com/hebermizuno/teste
+
+//verificar se assinou corretamente o diretório na nuvem.
+//git remote -v
+
